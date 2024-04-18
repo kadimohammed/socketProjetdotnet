d6c900dbcfbd3c17bb91c5cc8f7f9eb3f9b10004
kadimohammed <kaditgv06@gmail.com>
2024-04-18 22:01:40 +0100

    contact modification

---
+++ Socket_Projet_Client_Serveur/Socket_Projet_Server/Migrations/20240418201303_ajouter_models_message.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Socket_Projet_Server.Migrations
+{
+    public partial class ajouter_models_message : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenderId = table.Column<int>(type: "int", nullable: false),
+                    ReceiverId = table.Column<int>(type: "int", nullable: false),
+                    ConversationId = table.Column<int>(type: "int", nullable: false),
+                    SendDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Audio = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Video = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Message_Utilisateurs_ReceiverId",
+                        column: x => x.ReceiverId,
+                        principalTable: "Utilisateurs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Message_Utilisateurs_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "Utilisateurs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_ReceiverId",
+                table: "Message",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_SenderId",
+                table: "Message",
+                column: "SenderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+        }
+    }
+}
---
+++ Socket_Projet_Client_Serveur/Socket_Projet_Server/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socket_Projet_Server.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public int ConversationId { get; set; }
+        public DateTime SendDate { get; set; }
+        public Utilisateur Sender { get; set; }
+        public Utilisateur Receiver { get; set; }
+        public byte[] Image { get; set; } 
+        public byte[] Audio { get; set; }
+        public byte[] Video { get; set; }
+    }
+}
